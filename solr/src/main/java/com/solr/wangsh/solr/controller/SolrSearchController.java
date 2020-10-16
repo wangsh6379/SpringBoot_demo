2d375e05f409367b5870d48e619b76f3ab987537
909422229@qq.com <hui8878616>
2020-10-16 11:24:55 +0800

    spring-boot-solr:增加模糊查询与between查询

--- solr/src/main/java/com/solr/wangsh/solr/controller/SolrSearchController.java
+++ solr/src/main/java/com/solr/wangsh/solr/controller/SolrSearchController.java
@@ -7,9 +7,7 @@ import org.apache.solr.common.SolrInputDocument;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.solr.core.SolrTemplate;
-import org.springframework.data.solr.core.query.Criteria;
-import org.springframework.data.solr.core.query.SimpleQuery;
-import org.springframework.data.solr.core.query.SolrDataQuery;
+import org.springframework.data.solr.core.query.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -31,24 +29,25 @@ public class SolrSearchController {
     @RequestMapping("queryById")
     public Student queryById(@RequestParam String id) {
         Student student = solrSearchService.queryById(id);
-        System.out.println("ID:"+student.getId()+",姓名："+student.getName()+"，年龄："+student.getAge()+"，性别："+student.getSex());
+        System.out.println("ID:" + student.getId() + ",姓名：" + student.getName() + "，年龄：" + student.getAge() + "，性别：" + student.getSex());
         return null;
     }
 
     /**
      * 查询，通过条件进行查询
+     *
      * @param id
      * @return
      */
     @GetMapping("/query")
     public List<Student> text(@RequestParam String id) {
         //指定查询索引
-        SimpleQuery sq=new SimpleQuery("name:小");
+        SimpleQuery sq = new SimpleQuery("name:唱歌");
         //查到查询数据
         Page<Student> wangsh = solrTemplate.query("wangsh", sq, Student.class);
         List<Student> content = wangsh.getContent();
-        for (Student student:content) {
-            System.out.println("ID:"+student.getId()+",姓名："+student.getName()+"，年龄："+student.getAge()+"，性别："+student.getSex());
+        for (Student student : content) {
+            System.out.println("ID:" + student.getId() + ",姓名：" + student.getName() + "，年龄：" + student.getAge() + "，性别：" + student.getSex());
         }
         System.out.println(content.size());
         return null;
@@ -58,7 +57,7 @@ public class SolrSearchController {
      * 以ID为主键，如果存在则更新，如果不存在则新增
      */
     @GetMapping("/updateById")
-    public void updateById(){
+    public void updateById() {
         Student student = new Student();
         student.setAge(100);
         student.setName("小明");
@@ -71,11 +70,11 @@ public class SolrSearchController {
 
     /**
      * 增加字段，到指定的分区中。
-     *   <field name="content_id" type="plongs"/>
-//     <field name="id" type="string" multiValued="false" indexed="true" required="true" stored="true"/>
-//     <field name="name" type="string" uninvertible="false" indexed="true" stored="true"/>
-//     <field name="sex" type="string" uninvertible="false" indexed="true" stored="true"/>
-//     <field name="type" type="plongs"/>
+     * <field name="content_id" type="plongs"/>
+     * //     <field name="id" type="string" multiValued="false" indexed="true" required="true" stored="true"/>
+     * //     <field name="name" type="string" uninvertible="false" indexed="true" stored="true"/>
+     * //     <field name="sex" type="string" uninvertible="false" indexed="true" stored="true"/>
+     * //     <field name="type" type="plongs"/>
      */
     @GetMapping("/testUpdateDoc2")
     public void testUpdateDoc2() {
@@ -93,8 +92,8 @@ public class SolrSearchController {
      * 根据ID删除数据,可删除多个
      */
     @GetMapping("/deleteByIds")
-    public  void deleteByIds() {
-        solrTemplate.deleteByIds("wangsh", Arrays.asList("2","3"));
+    public void deleteByIds() {
+        solrTemplate.deleteByIds("wangsh", Arrays.asList("2", "3"));
         solrTemplate.commit("wangsh");
     }
 
@@ -102,12 +101,51 @@ public class SolrSearchController {
      * 根据条件进行删除，可满足多个条件即可删除数据
      */
     @GetMapping("/deleteById")
-    public  void deleteById() {
+    public void deleteById() {
         SolrDataQuery query = new SimpleQuery();
         query.addCriteria(Criteria.where("name").startsWith("小明"));
         //如果多加一个条件，类似于 SQL中 AND
         query.addCriteria(Criteria.where("sex").startsWith("男"));
-        solrTemplate.delete("wangsh",query);
+        solrTemplate.delete("wangsh", query);
         solrTemplate.commit("wangsh");
+    }
+
+    /**
+     * 单字段模糊查询
+     */
+    @GetMapping("/likeQuery")
+    public void likeQuery() {
+        SimpleQuery query = new SimpleQuery();
+        query.addCriteria(Criteria.where("sex").contains("女"));
+        Page<Student> wangsh = solrTemplate.query("wangsh", query, Student.class);
+        for (Student student : wangsh) {
+            System.out.println("ID:" + student.getId() + ",姓名：" + student.getName() + "，年龄：" + student.getAge() + "，性别：" + student.getSex());
+        }
+    }
+
+    /**
+     * 多字段模糊查询
+     */
+    @GetMapping("/andQuery")
+    public void andQuery() {
+        SimpleQuery query = new SimpleQuery();
+        query.addCriteria(Criteria.where("sex").and("name").contains("小"));
+        Page<Student> wangsh = solrTemplate.query("wangsh", query, Student.class);
+        for (Student student : wangsh) {
+            System.out.println("ID:" + student.getId() + ",姓名：" + student.getName() + "，年龄：" + student.getAge() + "，性别：" + student.getSex());
+        }
+    }
+
+    /**
+     * 在XXX之间，包含前后。模糊查询
+     */
+    @GetMapping("/betweenQuery")
+    public void betweenQuery() {
+        SimpleQuery query = new SimpleQuery();
+        query.addCriteria(Criteria.where("id").between("1","3"));
+        Page<Student> wangsh = solrTemplate.query("wangsh", query, Student.class);
+        for (Student student : wangsh) {
+            System.out.println("ID:" + student.getId() + ",姓名：" + student.getName() + "，年龄：" + student.getAge() + "，性别：" + student.getSex());
+        }
     }
 }
